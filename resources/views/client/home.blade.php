b413e3b651d257d860768f3e8052349c77257b94
KayK <kaykdsg@gmail.com>
2021-10-21 15:47:44 -0300

    finalizando header main e header comum

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -5,6 +5,55 @@
 
 @section('content')
 <div class="w-full">
-    @include('layouts/header')
+    @include('layouts/headerMain')
+    <div class="w-screen mt-10 flex flex-col items-center">
+        <h1 class="text-4xl font-main font-bold">Lançamentos</h1>
+        <div class="mt-10 ml-2 mr-2 flex flex-col md:flex-row md:justify-between md:w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+            <div class="w-80 border-2 border-gray-300 rounded-3xl flex flex-col mb-4">
+                <div class="w-72 self-center mt-3">
+                    <img src="https://3494.cdn.simplo7.net/static/3494/sku/quinzentenaconcentraco-camisa-branca-lisa-com-elastico-nas-mangas--p-1517426468386.jpg" alt="" class="rounded-3xl" >
+                </div>
+                <div class="mt-2 ml-4 mb-2">
+                    <span class="font-main text-gray-500 text-sm">Anime | Geek</span>
+                    <p class="mt-4 font-main text-lg font-light">Camiseta anime | Ahegao</p>
+                    <p class="mt-2 font-main text-xl font-medium">R$ 59,90</p>
+                    <span class="font-main text-gray-500 text-xs font-light">(até 6x sem juros)</span>
+                </div>
+            </div>
+            <div class="w-80 border-2 border-gray-300 rounded-3xl flex flex-col mb-4">
+                <div class="w-72 self-center mt-3">
+                    <img src="https://3494.cdn.simplo7.net/static/3494/sku/quinzentenaconcentraco-camisa-branca-lisa-com-elastico-nas-mangas--p-1517426468386.jpg" alt="" class="rounded-3xl" >
+                </div>
+                <div class="mt-2 ml-4 mb-2">
+                    <span class="font-main text-gray-500 text-sm">Anime | Geek</span>
+                    <p class="mt-4 font-main text-lg font-light">Camiseta anime | Ahegao</p>
+                    <p class="mt-2 font-main text-xl font-medium">R$ 59,90</p>
+                    <span class="font-main text-gray-500 text-xs font-light">(até 6x sem juros)</span>
+                </div>
+            </div>
+            <div class="w-80 border-2 border-gray-300 rounded-3xl flex flex-col mb-4">
+                <div class="w-72 self-center mt-3">
+                    <img src="https://3494.cdn.simplo7.net/static/3494/sku/quinzentenaconcentraco-camisa-branca-lisa-com-elastico-nas-mangas--p-1517426468386.jpg" alt="" class="rounded-3xl" >
+                </div>
+                <div class="mt-2 ml-4 mb-2">
+                    <span class="font-main text-gray-500 text-sm">Anime | Geek</span>
+                    <p class="mt-4 font-main text-lg font-light">Camiseta anime | Ahegao</p>
+                    <p class="mt-2 font-main text-xl font-medium">R$ 59,90</p>
+                    <span class="font-main text-gray-500 text-xs font-light">(até 6x sem juros)</span>
+                </div>
+            </div>
+            <div class="w-80 border-2 border-gray-300 rounded-3xl flex flex-col mb-4">
+                <div class="w-72 self-center mt-3">
+                    <img src="https://3494.cdn.simplo7.net/static/3494/sku/quinzentenaconcentraco-camisa-branca-lisa-com-elastico-nas-mangas--p-1517426468386.jpg" alt="" class="rounded-3xl" >
+                </div>
+                <div class="mt-2 ml-4 mb-2">
+                    <span class="font-main text-gray-500 text-sm">Anime | Geek</span>
+                    <p class="mt-4 font-main text-lg font-light">Camiseta anime | Ahegao</p>
+                    <p class="mt-2 font-main text-xl font-medium">R$ 59,90</p>
+                    <span class="font-main text-gray-500 text-xs font-light">(até 6x sem juros)</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
---
+++ resources/views/layouts/headerMain.blade.php
@@ -0,0 +1,25 @@
+<div class="flex flex-col w-full  rounded-b-md" style="background-color: #1F2229; height: 26rem">
+    <div class="flex flex-row justify-between items-center w-full mt-5 pb-6">
+        <img src="https://tccflama2021.s3.us-east-2.amazonaws.com/products_images/nomeSemLogo.png" alt="nome" class="ml-10 w-36">
+        <form action="" class="hidden md:flex md:max-w-md lg:max-w-2xl xl:max-w-3xl 2xl:max-w-6xl w-full">
+            <div class="flex justify-center items-center w-full bg-white rounded-md p-1">
+                <input type="text" class="w-auto border-0 focus:ring-transparent w-full font-main text-black " placeholder="Camisa naruto">
+                <button class="text-xl md:text-2xl"><i class="fas fa-search"></i></button>
+            </div>
+        </form>
+        <div class="mr-10">
+            <a href="" class="text-white mr-3 text-xl md:text-2xl"><i class="fas fa-user"></i></a>
+            <a href="" class="text-white text-xl md:text-2xl"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+    </div>
+    <div class="bgMainHome w-full" style="background: url(https://tccflama2021.s3.us-east-2.amazonaws.com/products_images/bg2.jpg); height: 26rem; background-repeat: no-repeat; background-size: cover; background-position: center; background-attachment: fixed">
+        <div class="absolute right-0 left-0 bottom-0 top-16">
+            <div class="w-full flex justify-between items-center mt-2 bg-white md:mt-7 h-8 text-sm md:h-10 md:text-lg rounded-b-md">
+                <a href="camisas" class="ml-3 md:ml-32 font-main">Camisas</a>
+                <a href="moletons" class="font-main">Moletons</a>
+                <a href="acessorios" class="font-main">Acessórios</a>
+                <a href="canecas" class="mr-3 md:mr-32 font-main">Canecas</a>
+            </div>
+        </div>
+    </div>
+</div>
